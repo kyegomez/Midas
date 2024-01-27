de33b0d248aa4b2e52dd244a39ce68f28e0d91b6
Kye <kye@apacmediasolutions.com>
2024-01-26 19:29:48 -0500

    [README]

---
+++ midas/main.py
@@ -0,0 +1,11 @@
+import torch 
+from torch import nn, Tensor
+from einops import rearrange, reduce, repeat
+
+
+class MiDaS:
+    def __init__(self):
+        pass
+    
+    def run(self, text: Tensor):
+        pass
